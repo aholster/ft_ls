b67374e34c218d4b0912fea3483a180cf03fb366
aholster <aholster@student.codam.nl>
2019-09-12 21:11:04 +0200

    project groundwork

---
+++ ft_ls.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_ls.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: aholster <aholster@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/09/12 19:44:46 by aholster       #+#    #+#                */
+/*   Updated: 2019/09/12 20:04:33 by aholster      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS
+# define FT_LS
+
+//# include <unistd.h>
+//# include <stdlib.h>
+# include "./../clean_printf/ft_printf.h"
+
+// # include <dirent.h>
+// # include <sys/stat.h>
+
+// # include <sys/types.h>
+// # include <pwd.h>
+// # include <uuid/uuid.h>
+
+// # include <grp.h>
+
+// # include <sys/xattr.h>
+
+// # include <time.h>
+
+// # include <stdio.h>
+
+
+#endif
